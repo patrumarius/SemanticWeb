1a11e48f8b4de4e71eaa5a1d5e303673217b1045
Marius-Aurelian Patru <Marius.Patru@ibm.com>
2019-01-09 07:49:31 +0200

    solde

--- src/test/java/pages/paneaugauche/MemberAccountPage.java
+++ src/test/java/pages/paneaugauche/MemberAccountPage.java
@@ -9,6 +9,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.qameta.allure.Step;
 
@@ -28,6 +30,8 @@ public class MemberAccountPage {
 	
 	@Step("Verifier le solde du compte")
 	public MemberAccountPage verifierSoldeCompte() {
+		WebDriverWait wait = new WebDriverWait(driver,10, 100);
+		wait.withMessage("error").until(ExpectedConditions.visibilityOf(solde));
 		System.out.println(solde.getText());
 		assertTrue(solde.getText().equals("Balance: 5.112,94 IU's"));
 		return new MemberAccountPage(driver);
